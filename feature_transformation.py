3cdce1289fa25f402c5b257ef34a9855e9815696
lucas-lr <lucas@lead-ratings.com>
2018-08-26 18:16:42 +0200

    Add feature_transformation.py with the  transform_features function.

---
+++ feature_transformation.py
@@ -0,0 +1,73 @@
+from sklearn.preprocessing import LabelEncoder, Imputer, MinMaxScaler
+from TFM.feature_engineering import clip_continuous_f
+
+
+def transform_features(df0, con_cols=[], lstm_cols={}, cat_cols={},
+                       drop_cols=True, verbose=True):
+    '''
+    Returns the dataframe with the transformed features and M.
+    Applies 2 transformers to the continuous and LSTM features:
+     > Imputer(strategy='median')
+     > MinMaxScaler()
+    Clips LSTM features before fitting the MinMaxScaler().
+    Applies a label encoder to the categorical columns.
+    Parameters:
+     > con_cols: list of continuous columns
+     > lstm_cols: dict of LSTM continuous columns (keys) and their lower and
+       upper clip values (values).
+     > cat_cols: dict of categorical columns (keys) and
+       the number of features to be returned.
+     > drop_na: if True, the original columns are excluded.
+    '''
+    
+    df = df0.copy()
+
+    # Preprocessing numerical features
+    if verbose:
+        print('> Preprocessing continuous features...')
+    con_features = []
+    for f in con_cols:
+        t1 = Imputer(strategy='median')
+        t2 = MinMaxScaler()
+        df['con__' + f] = t1.fit_transform(df[[f]])
+        df['con__' + f] = t2.fit_transform(df[['con__' + f]])
+        con_features.append('con__' + f)
+        if verbose:
+            print('  > {}'.format(f))
+    
+    if verbose:
+        print()
+        print('> Preprocessing LSTM continuous features...')
+    lstm_features = []
+    for f in lstm_cols.keys():
+        t1 = Imputer(strategy='median')
+        t2 = MinMaxScaler()
+        df['con__' + f] = t1.fit_transform(df[[f]])
+        t2.fit(df[['con__' + f]].clip(lstm_cols[f][0],
+                                      lstm_cols[f][1]))
+        df['con__' + f] = t2.transform(df[['con__' + f]])
+        lstm_features.append('con__' + f)
+        if verbose:
+            print('  > {}'.format(f))
+
+    # Preprocessing categorical features
+    if verbose:
+        print()
+        print('> Preprocessing categorical features...')
+    cat_features = []
+    M = []
+
+    for f in cat_cols.keys():
+        le = LabelEncoder()
+        df['cat__' + f] = le.fit_transform(df[f])
+        m = df['cat__' + f].max() + 1
+        cat_features.append('cat__' + f)
+        M.append((m, cat_cols[f]))
+        if verbose:
+            print('  > {}'.format(f))
+    
+    if drop_cols:
+        df.drop(columns=con_cols + list(lstm_cols.keys()) + list(cat_cols.keys()),
+                inplace=True)
+        
+    return df, con_features, lstm_features, cat_features, M
