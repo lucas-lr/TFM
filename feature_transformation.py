e9b4e6999fc400297d5e37f9e1c58b5fa2f1b3d2
lucas-lr <lucas@lead-ratings.com>
2018-08-29 20:42:22 +0200

    Improve transform_features in order to optimize efficiency.

--- feature_transformation.py
+++ feature_transformation.py
@@ -2,20 +2,28 @@ from sklearn.preprocessing import LabelEncoder, Imputer, MinMaxScaler
 from TFM.feature_engineering import clip_continuous_f
 
 
-def transform_features(df0, con_cols=[], lstm_cols={}, cat_cols={},
+def has_nulls(s):
+    return s.isnull().values.any()
+
+
+def is_binary(s):
+    return sorted(s.unique()) == [0, 1]
+
+
+def transform_features(df0, con_cols=[], lstm_cols=[], cat_cols={},
     strategy='median', drop_cols=True, verbose=True):
     '''
     Returns:
-     > dataframe with the transformed features
+     > dataframe with transformed features
      > feat_dict with lists of continuous, LSTM and categorical features and M
-    Applies 2 transformers to the continuous and LSTM features:
+    Optionally, it applies 2 transformers to the continuous and LSTM features:
      > Imputer()
      > MinMaxScaler()
     Clips LSTM features before fitting the MinMaxScaler().
     Applies a label encoder to the categorical columns.
     Parameters:
      > con_cols: list of continuous columns
-     > lstm_cols: dict of LSTM continuous columns (keys) and their lower and
+     > lstm_cols: list / dict of LSTM continuous columns (keys) and their lower and
        upper clip values (values).
      > cat_cols: dict of categorical columns (keys) and
        the number of features to be returned.
@@ -30,25 +38,43 @@ def transform_features(df0, con_cols=[], lstm_cols={}, cat_cols={},
         print('> Preprocessing continuous features...')
     con_features = []
     for f in con_cols:
-        t1 = Imputer(strategy=strategy)
-        t2 = MinMaxScaler()
-        df['con__' + f] = t1.fit_transform(df[[f]])
-        df['con__' + f] = t2.fit_transform(df[['con__' + f]])
+        if has_nulls(df[f]):
+            t1 = Imputer(strategy=strategy)
+            df['con__' + f] = t1.fit_transform(df[[f]])
+        else:
+            df['con__' + f] = df[f]
+        if not is_binary(df[f]):
+            t2 = MinMaxScaler()
+            df['con__' + f] = t2.fit_transform(df[['con__' + f]])
         con_features.append('con__' + f)
         if verbose:
             print('  > {}'.format(f))
-    
+
     if verbose:
         print()
         print('> Preprocessing LSTM continuous features...')
     lstm_features = []
-    for f in lstm_cols.keys():
-        t1 = Imputer(strategy=strategy)
-        t2 = MinMaxScaler()
-        df['con__' + f] = t1.fit_transform(df[[f]])
-        t2.fit(df[['con__' + f]].clip(lstm_cols[f][0],
-                                      lstm_cols[f][1]))
-        df['con__' + f] = t2.transform(df[['con__' + f]])
+
+    clip = isinstance(lstm_cols, dict)
+    if clip:
+        cols = lstm_cols.keys()
+    else:
+        cols = lstm_cols
+    for f in cols:
+        if has_nulls(df[f]):
+            t1 = Imputer(strategy=strategy)
+            df['con__' + f] = t1.fit_transform(df[[f]])
+        else:
+            df['con__' + f] = df[f]
+        if is_binary(df[f]):
+            if clip:
+                s = df[['con__' + f]].clip(lstm_cols[f][0],
+                                           lstm_cols[f][1])
+            else: s = df[['con__' + f]]
+            t2 = MinMaxScaler()
+            t2.fit(s)
+            df['con__' + f] = t2.transform(df[['con__' + f]])
+
         lstm_features.append('con__' + f)
         if verbose:
             print('  > {}'.format(f))
