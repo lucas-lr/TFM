d0808b4c74f45d88246a32af24a4111f6f2a2d14
lucas-lr <lucas@lead-ratings.com>
2018-09-11 17:38:45 +0200

    Fix plots.py issues.

--- plots.py
+++ plots.py
@@ -26,23 +26,23 @@ def plot_binary_roc_curve(y_test, y_score):
 def plot_adjustment(result, target_name='y', score_name='score', groups=10,
                     lift=False, display_table=False):
     n = np.ceil(len(result)/groups)
-    result.sort_values('score', inplace=True)
+    result.sort_values(score_name, inplace=True)
     result.reset_index(drop=True, inplace=True)
     result['group'] = np.floor((result.index)/n).astype(int) + 1
     groups = result.groupby('group')
     grouped_R = groups.mean()
     grouped_R['size'] = groups.size()
     
-    ymean = grouped_R['y'].mean()
-    smean = grouped_R['score'].mean()
+    ymean = grouped_R[target_name].mean()
+    smean = grouped_R[score_name].mean()
     
-    grouped_R['real_lift'] = grouped_R['y'] / ymean
-    grouped_R['pred_lift'] = grouped_R['score'] / smean
+    grouped_R['real_lift'] = grouped_R[target_name] / ymean
+    grouped_R['pred_lift'] = grouped_R[score_name] / smean
     
     if lift:
         grouped_R[['pred_lift', 'real_lift']].plot(marker='.')
     else:
-        grouped_R[['score', 'y']].plot(marker='.')
+        grouped_R[[score_name, target_name]].plot(marker='.')
     
     if display_table:
         display(grouped_R)
